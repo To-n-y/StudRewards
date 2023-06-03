980c3621403a2c6f481e4dce7369a06d8dd5adcb
Tony <realvitl@yandex.ru>
2023-06-03 15:54:41 +0300

    update views

--- StudRewards/mainapp/views.py
+++ StudRewards/mainapp/views.py
@@ -1,19 +1,25 @@
+from django.http import HttpResponse, HttpResponsePermanentRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template.response import TemplateResponse
+
+from .forms import LoginForm
+from .models import Teacher
 
 
 def index(request):
-    header = "Заголовок"  # обычная переменная
-    message = ["python", "django"]  # список
-
-    data = {"header": header, "message": message}
-    return TemplateResponse(request, "index.html", data)
+    return HttpResponsePermanentRedirect("/login")
 
 
-def about(request):
-    return HttpResponse("О сайте")
+def login(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        teachers = Teacher.objects.all()
+        teacher = teachers.filter(email=email).first()
+        if teacher is None:
+            return HttpResponse(
+                f"<h2>Привет, незнакомец! <a href="">На главную</a> </h2>")
+        return HttpResponse(f"<h2>Привет, {teacher.firstName}, твой пароль: {password} <a href="">На главную</a> </h2>")
 
-
-def contact(request):
-    return HttpResponse("Контакты")
+    else:
+        loginform = LoginForm(initial={'password': 'pass'})
+        return render(request, "login.html", {"form": loginform})

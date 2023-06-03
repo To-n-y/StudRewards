4ab0550542885b4d4b38f39946a4564cf2b369a7
Tony <realvitl@yandex.ru>
2023-06-03 22:59:45 +0300

    update forms

--- StudRewards/mainapp/forms.py
+++ StudRewards/mainapp/forms.py
@@ -2,5 +2,16 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    email = forms.CharField()
-    password = forms.CharField()
+    email = forms.CharField(label="email:")
+    password = forms.CharField(label="password:")
+
+
+class ReportForm(forms.Form):
+    description = forms.CharField()
+    choice = [
+        (1, "first"),
+        (2, "second"),
+    ]
+    activity = forms.ChoiceField(label="activities", choices=choice)
+    students = forms.MultipleChoiceField(label="students", choices=choice)
+

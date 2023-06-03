fa9c6df55552cb4f7b60a5eb13a23393903c7443
Tony <realvitl@yandex.ru>
2023-06-03 23:01:17 +0300

    update urls & views

--- StudRewards/StudRewards/urls.py
+++ StudRewards/StudRewards/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('login/', views.login, name='mainapp'),
     path('', views.index, name='mainapp'),
+    path('teacher/', views.teacherProfile, name='mainapp'),
 
 ]
--- StudRewards/mainapp/views.py
+++ StudRewards/mainapp/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, HttpResponsePermanentRedirect
 from django.shortcuts import render
 
-from .forms import LoginForm
+from .forms import LoginForm, ReportForm
 from .models import Teacher
 
 
@@ -21,5 +21,23 @@ def login(request):
         return HttpResponse(f"<h2>Привет, {teacher.firstName}, твой пароль: {password} <a href="">На главную</a> </h2>")
 
     else:
-        loginform = LoginForm(initial={'password': 'pass'})
-        return render(request, "login.html", {"form": loginform})
+        loginForm = LoginForm(initial={'password': 'pass'})
+        return render(request, "login.html", {"loginForm": loginForm})
+
+
+def teacherProfile(request):
+    if request.method == "POST":
+        description = request.POST.get("description")
+        activity = request.POST.get("activity")
+        students = request.POST.getlist("students")
+        teachers = Teacher.objects.all()
+        teacher = teachers.filter(email="mail@mail.ru").first()
+        if teacher is None:
+            return HttpResponse(
+                f"<h2>Привет, незнакомец! <a href="">На главную</a> </h2>")
+        print("STUDENTS ", students)
+        return HttpResponse(f"<h2>Привет, , твой пароль:  <a href="">На главную</a> </h2>")
+
+    else:
+        reportForm = ReportForm()
+        return render(request, "teacherProfile.html", {"reportForm": reportForm})
